11b22f3c5d15815e95b10b90e16301074cd6e114
momoDevDL <mohamed.masbah-abou-laich@master-developpement-logiciel.fr>
2021-02-12 12:52:47 +0100

    patch document and section creation / add persmission handling code and modify the document handler to handle file uploads

--- backend/api/Controller/CourseController.js
+++ backend/api/Controller/CourseController.js
@@ -6,42 +6,122 @@ export function createSection(req,res){
 
     var sectionName = req.body.name ;
     var courseId = req.body.courseId;
+    //var userRoleId = req.body.userRoleId;
     
 
     if( sectionName == null || courseId == null){
         return res.status(400).send({error :  "missing field ! can't carry on with your request"});
     }
 
-    const newSection =  models.section.create({
-        name: sectionName,
-        courseID : courseId   
-    }).then( (newSection)=> {
-        console.log(newSection.name + "created");
-        return res.status(200).send({
-            section : newSection
-        })
-    }).catch( (err) => {
-        return res.status(500).send({
-            error: err + "create request error"
-        });
+    // preparer l'intégration de la vérification des permissions
+
+  /*  models.role_has_permission.findAll({
+        attributes:['permissionID'],
+        where:{
+            roleID : userRoleId
+        }
+    }).then((listOfPermissions)=>{
+        
+            models.permission.findOne({
+                where : {
+                    id : pid,
+                    name : "creation"
+                }
+            }).then((permission)=>{
+                let i = 0 ;
+                while(listOfPermissions && listOfPermissions[i].name != "creation"){
+                       i++;
+                }
+                if(i == length(listOfPermissions))
+                return res.send({error:" user does not have the right to create section"})
+                
+            })
+    
+    })*/
+
+    models.course.findOne({
+        attributes : ['id'],
+        where : {
+            id : courseId
+        }
+    }).then((courseFound) =>{
+        if(courseFound){
+
+            const newSection =  models.section.create({
+                name: sectionName,
+                courseID : courseId   
+            }).then( (newSection)=> {
+                console.log(newSection.name + "created");
+                return res.status(200).send({
+                    section : newSection
+                })
+            }).catch( (err) => {
+                return res.status(500).send({
+                    error: err + "create request error"
+                });
+            })
+            
+        }else{
+            return res.send({error : "the Id course doesn't match any existing course! please try again "});
+        }
+    }).catch((err)=>{
+        return res.send({error : "DB request failure cannot find matching course ID"})
     })
+    
 };
 
 export function createDocument(req,res){
-    var documentName = req.body.name ;
-    var message = req.body.message;
-    var filepath = req.body.filepath;
-    var sectionID = req.body.sectionID;
+    let message = req.body.message;
+    let sectionID = req.body.sectionID;
+    let document;
+    let uploadPath;
+    //var userRoleId = req.body.userRoleId;
 
-    if( documentName == null || message == null || filepath == null || sectionID == null){
+    if (!req.files || Object.keys(req.files).length === 0 || message == null ||sectionID == null) {
         return res.status(400).send({error : "missing field ! can't carry on with your request"});
     }
 
+    // preparer l'intégration de la vérification des permissions
+
+  /*  models.role_has_permission.findAll({
+        attributes:['permissionID'],
+        where:{
+            roleID : userRoleId
+        }
+    }).then((listOfPermissions)=>{
+        
+            models.permission.findOne({
+                where : {
+                    id : pid,
+                    name : "creation"
+                }
+            }).then((permission)=>{
+                let i = 0 ;
+                while(listOfPermissions && listOfPermissions[i].name != "creation"){
+                       i++;
+                }
+                if(i == length(listOfPermissions))
+                return res.send({error:" user does not have the right to create section"})
+                
+            })
+    
+    })*/
+    
+    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+    document = req.files.document;
+    uploadPath = __dirname + '../../uploads/' + document.name;
+
+    // Use the mv() method to place the file somewhere on your server
+    document.mv(uploadPath, function (err) {
+        if (err)
+            return res.status(500).send(err);
+    });
+
     
     models.document.create({
-        name: sectionName,
+        name: document.name,
         message : message,
-        filepath: filepath,
+        filepath: uploadPath,
         sectionID : sectionID   
     }).then( (newDocument)=> {
 
--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,14 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors'
 import apiRoutes from "./api/api-routes"
+const fileUpload = require('express-fileupload');
 require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT;
 
 app.use(bodyParser.json());
-
+app.use(fileUpload());
 app.use(cors())
 
 app.use('/api', apiRoutes);
